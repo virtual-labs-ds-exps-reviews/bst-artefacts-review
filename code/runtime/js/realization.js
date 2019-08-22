4a920e465dd51f6f9a87eb3889ec15e6f53adedf
sadhana <sadhana.virupaksha@gmail.com>
2019-08-22 17:10:39 +0530

    add code

---
+++ code/runtime/js/realization.js
@@ -0,0 +1,101 @@
+
+var realizationCatalog = {
+  "Picture: Terminologies": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/terms.png",
+    "height": "50%",
+    "width": "50%"
+  } ,
+  "Picture: BST Property": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/property.png",
+    "height": "100%",
+    "width": "50%"
+  } ,
+  "Picture: BST Node Structure": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/structure.png",
+    "height": "100%",
+    "width": "35%"
+  } ,
+  "Picture: BST sort": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/treesort.png",
+    "height": "100%",
+    "width": "65%"
+  } ,
+  "Video: BST Property": {
+    "resource_type": "video",
+    "url": "/build/code/static/videos/BSTProperties.mp4"
+  } ,
+  "Video: Searching": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/u4kEcfh5PVg"
+  } ,
+  "Video: Insertion": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/aMNsx-L-M7k"
+  } ,
+  "Video: Traversal": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/DQT9fkegcoQ"
+  } ,
+  "Video: Sorting": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/hLYfGcJjB-I"
+  } ,
+  "Video: Deletion": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/TluDFUhxmAo"
+  } ,
+  "Exercise: BST Search": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/BSTsearch.html"
+  },
+  "BST Search Demo": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bstSearch.html"
+  },
+  "BST Search Practice": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bstSearchPractice.html"
+  },
+  "BST Insert Demo": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bstInsert.html"
+  },
+  "BST Delete Demo": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bstDelete.html"
+  },
+  "BST Delete Exercise": {
+    "reqs_satisfied": "provides an interactive exercise artefact for searching a node of a Binary Serach Tree after the learner gets familiar with the Binary Search Property",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bstDeleteExercise.html"
+  },
+   "BST Insertion": {
+    "reqs_satisfied": "provides an interactive demonstration artefact for construction of a Binary Serach Tree",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/BSTconstruction.html"
+  },
+  "BST Traversal": {
+    "reqs_satisfied": "provides an interactive artefact demonstrating the traversal across the Binary Search Tree",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/BSTtraversal.html"
+  } ,
+  "Exercise: BST Sort": {
+    "reqs_satisfied": "provides an interactive artefact demonstrating the inorder traversal across the Binary Search Tree to sort the nodes in the tree.",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/BSTsort.html"
+  },
+  "BST Deletion": {
+    "reqs_satisfied": "provides an interactive artefact demonstrating the deletion from the Binary Search Tree.",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/BSTdelete.html"
+  },
+}
